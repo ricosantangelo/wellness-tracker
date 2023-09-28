be2e66529846abae6522992c33bd8fda19e1fcb7
Erik Martinez <132791614+ejm730@users.noreply.github.com>
2023-09-28 11:51:28 -0700

    fixed conversation creation logic for new user

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -31,40 +31,42 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
     
-    // Signup Form Submission
-    const signupForm = document.querySelector('#signup-form');
-    if (signupForm) {
-      signupForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        
-        const firstName = document.querySelector('#signup-fname').value;
-        const lastName = document.querySelector('#signup-lname').value;
-        const email = document.querySelector('#signup-email').value;
-        const password = document.querySelector('#signup-pwd').value;
-        
-        try {
-          const response = await fetch('/register', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'same-origin', // Ensure cookies are sent with the request
-            body: JSON.stringify({ firstName, lastName, email, password })
-          });
-          
-          if (response.ok) {
-            // Set a flag in sessionStorage to indicate that the user just registered
-            sessionStorage.setItem('userJustRegistered', 'true');
-            // Redirect to chat page if registration is successful
-            window.location.href = '/chat';
-          } else {
-            // Display error message
-            const data = await response.json();
-            alert(data.error);
-          }
-        } catch (error) {
-          console.error('Error:', error);
-        }
+   // Signup Form Submission
+const signupForm = document.querySelector('#signup-form');
+if (signupForm) {
+  signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault(); 
+    
+    const firstName = document.querySelector('#signup-fname').value;
+    const lastName = document.querySelector('#signup-lname').value;
+    const email = document.querySelector('#signup-email').value;
+    const password = document.querySelector('#signup-pwd').value;
+    
+    try {
+      const response = await fetch('/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'same-origin', // Ensure cookies are sent with the request
+        body: JSON.stringify({ firstName, lastName, email, password })
       });
+      
+      if (response.ok) {
+        // Instead of redirecting, show a success message or modal
+        alert('User registered successfully! Please log in.');
+        
+        // Optionally, you can clear the form fields
+        signupForm.reset();
+      } else {
+        // Display error message
+        const data = await response.json();
+        alert(data.error);
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
+  });
+}
+
     
   });
   
